82a91100fb4dd1ab3cb6454ee7f2fae156063748
Piotr <piotrwc49@gmail.com>
2025-07-24 19:06:10 +0200

    login and register forms

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,11 +3,35 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import Link from "next/link"
 import Image from "next/image";
+import { InferType, object, string } from "yup";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const loginForm = object({
+  email: string().email().required(),
+  password: string().required(),
+})
+
+export type LoginFormRequest = InferType<typeof loginForm>
 
 export default function LoginPage() {
+  const defaultValues = {
+    email: "",
+    password: "",
+  }
+  const form = useForm<LoginFormRequest>({
+    resolver: yupResolver(loginForm),
+    defaultValues,
+    mode: "onTouched",
+  })
+
+  const onSubmit = form.handleSubmit(async (requestData: LoginFormRequest)=>{
+    console.log(requestData)
+  })
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <Card className="w-full max-w-md">
@@ -16,19 +40,40 @@ export default function LoginPage() {
           <CardDescription className="text-center">Enter your credentials to access your account</CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="login">E-mail</Label>
-            <Input id="login" type="text" placeholder="Enter your login" required/>
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" placeholder="Enter your password" required/>
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
+          <Form {...form}>
+            <form onSubmit={onSubmit}>
+              <FormField
+                name={"email"}
+                render={({field})=>(
+                  <FormItem className="pb-3">
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+              )}
+              />
+              <FormField
+                name={"password"}
+                render={({field})=>(
+                  <FormItem className="pb-4">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </form>
+          </Form>
           <div className="w-full flex justify-center">
-            <Link className="underline text-sm font-semibold" href="/register">You don&apos;t have account. Sign in.</Link>
+            <Link className="underline text-sm font-semibold" href="/register">You don&apos;t have account. Sign
+              in.</Link>
           </div>
           <div className="flex flex-row items-center justify-center gap-2">
             <div className="border w-full"/>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,33 +1,89 @@
+"use client"
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { InferType, object, string } from "yup";
+
+const signUpForm = object({
+  email: string().email().required(),
+  password: string().required(),
+  repeatPassword: string().required(),
+})
+
+export type SignUpFormRequest = InferType<typeof signUpForm>
 
 export default function RegisterPage() {
+  const defaultValues = {
+    email: "",
+    password: "",
+    repeatPassword: "",
+  }
+  const form = useForm<SignUpFormRequest>({
+    resolver: yupResolver(signUpForm),
+    defaultValues,
+    mode: "onTouched",
+  })
+
+  const onSubmit = form.handleSubmit(async (requestData: SignUpFormRequest) => {
+    console.log(requestData)
+  })
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl font-bold text-center">Register</CardTitle>
+          <CardTitle className="text-2xl font-bold text-center">Sign up</CardTitle>
           <CardDescription className="text-center">Enter your credentials to access your account</CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="login">E-mail</Label>
-            <Input id="login" type="text" placeholder="Enter your login" required/>
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" placeholder="Enter your password" required/>
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Repeat password</Label>
-            <Input id="password" type="password" placeholder="Repeat your password" required/>
-          </div>
-          <Button type="submit" className="w-full">
-            Sign up
-          </Button>
+          <Form {...form}>
+            <form onSubmit={onSubmit}>
+              <FormField
+                name={"email"}
+                render={({ field }) => (
+                  <FormItem className="pb-3">
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name={"password"}
+                render={({ field }) => (
+                  <FormItem className="pb-4">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name={"repeatPassword"}
+                render={({ field }) => (
+                  <FormItem className="pb-4">
+                    <FormLabel>Repeat password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Sign up
+              </Button>
+            </form>
+          </Form>
           <div className="flex flex-row items-center justify-center gap-2">
             <div className="border w-full"/>
             <div className="px-5">Or</div>
